afc59408a032dab14cd54741020aae07f2d1c45a
Deepjyoti Barman <deep.barman30@gmail.com>
2020-10-27 16:43:30 +0530

    Add repo_handler as an endpoint
    Bind the repo_handler module to the repos/ endpoint in the server module

--- web/server.py
+++ web/server.py
@@ -3,7 +3,15 @@
 from fastapi import FastAPI
 from uvicorn import run
 
+from .repo_handler import RepoList, get_repo_list
+
 app = FastAPI()
+
+
+@app.get("/repos/{username}", response_model=RepoList)
+async def get_repos(username: str):
+    response = get_repo_list(username)
+    return response
 
 
 if __name__ == "__main__":
